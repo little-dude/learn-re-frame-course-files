950ae1ee04e96d15da51af0089bb691bcc64588c
Corentin Henry <corentinhenry@gmail.com>
2019-07-14 16:16:21 +0200

    06 - initialize-db

--- cheffy/src/app/core.cljs
+++ cheffy/src/app/core.cljs
@@ -1,17 +1,20 @@
 (ns app.core
   (:require [reagent.core :as r]
+            [re-frame.core :as rf]
             ["@smooth-ui/core-sc" :refer [Normalize Button ThemeProvider]]
-            [app.theme :refer [cheffy-theme]]))
+            [app.theme :refer [cheffy-theme]]
+            [app.db]))
 
 (defn app
   []
   [:<>
    [:> Normalize]
    [:> ThemeProvider {:theme cheffy-theme}
-    [:> Button {:variant "info"} "Hello"]]])
+    [:> Button {:variant "info"} "Hello!"]]])
 
 (defn ^:dev/after-load start
   []
+  (rf/dispatch-sync [:initialize-db])
   (r/render [app]
     (.getElementById js/document "app")))
 
