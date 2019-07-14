88a615a06b9a095886037b817f254b9cd55c0aae
Corentin Henry <corentinhenry@gmail.com>
2019-07-14 15:45:24 +0200

    05 - smooth-ui

--- cheffy/src/app/core.cljs
+++ cheffy/src/app/core.cljs
@@ -1,9 +1,14 @@
 (ns app.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            ["@smooth-ui/core-sc" :refer [Normalize Button ThemeProvider]]
+            [app.theme :refer [cheffy-theme]]))
 
 (defn app
   []
-  [:div "Cheffy"])
+  [:<>
+   [:> Normalize]
+   [:> ThemeProvider {:theme cheffy-theme}
+    [:> Button {:variant "info"} "Hello"]]])
 
 (defn ^:dev/after-load start
   []
